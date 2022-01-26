8e782555239c04d680a8c8f4ec95154440ec253e
j-dabling <jgdabble@zoho.com>
2022-01-26 15:06:10 -0700

    Removed name from player class.

--- hilo.py
+++ hilo.py
@@ -3,15 +3,12 @@
 
 # The Player class for the Hilo game.
 # player.points - The number of points for the player.
-# player.name - An optional name that can be entered on game start.
 # setPoints() - adds or subtracts a given amount from the player's score.
 # getPoints() - returns the player's points. (int)
 class player:
     points = 300
-    name = ''
 
-    def __init__(self, player_name = '', num_points = 300):
-        name = player_name
+    def __init__(self, num_points = 300):
         points = num_points
 
     # Adds/subtracts given amount of points (int) to/from player's total.
